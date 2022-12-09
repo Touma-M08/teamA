5eecd7e7721b25d86b63aaf83081db8f992bbbcb
a5pgm <azinori1212@gmail.com>
2022-12-09 22:44:17 +0900

    ログイン画面にregisterボタンの追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,6 +40,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <div>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="mr-3 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Register</a>
+                @endif
+                </div>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
