900b0d975e44ea03cd512f29db99a4cf963b3965
Touma-M08 <touma_064810@yahoo.co.jp>
2022-12-10 13:07:29 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2022_12_10_033526_create_places_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("address");
+            $table->string("tel");
+            $table->double('lat');
+            $table->double('lng');
+            $table->text("detail");
+            $table->text("review");
+            $table->foreignId("user_id")->constrained();
+            $table->foreignId("category_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+};
