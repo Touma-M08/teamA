3218f68f4390e10db2f3b1627888ae257e92d95b
Touma-M08 <touma_064810@yahoo.co.jp>
2022-12-11 15:04:24 +0900

    お気に入り登録（フロント）実装

--- resources/views/favorite.blade.php
+++ resources/views/favorite.blade.php
@@ -19,9 +19,6 @@
                 <a href="{{route('shopCreate')}}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">お店を登録する</a>
               </li>
               <li>
-                <a href="{{route('favorites')}}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">お気に入り一覧</a>
-              </li>
-              <li>
                 <form method="POST" action="{{ route('logout') }}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                     @csrf
                     <x-responsive-nav-link :href="route('logout')"
@@ -30,7 +27,9 @@
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
               </li>
-              
+              <li>
+                <a href="{{route('logout')}}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">お店を登録する</a>
+              </li>
             </ul>
         </div>
       @else
@@ -41,16 +40,7 @@
     </header>
     <!-- text - start -->
     <div class="mb-10 md:mb-16">
-      <form method="GET" action ="{{ url('/search') }}" >
-        @csrf
-        <div class="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5">
-          <input type = "search" placeholder="店名" name="shopName" class="flex-col inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"/>
-          <input type = "search" placeholder="エリア" name="adress" class="flex-col inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"/>
-        <button type = "submit" class="justify-center mt-3 flex-col inline-block bg-black hover:bg-gray-800 focus-visible:ring ring-indigo-300 text-white active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">絞り込み検索</button>
-        </div>
-        
-     </from>
-      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6 mt-5">検索結果</h2>
+      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6 mt-5">お気に入り一覧</h2>
 
       <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto"></p>
     </div>
@@ -58,7 +48,7 @@
 
     <div class="grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 md:gap-6 xl:gap-8">
       <!-- article - start -->
-      @foreach ($places as $place)
+      @foreach ($favorites as $favorite)
       <div class="flex flex-col md:flex-row items-center border rounded-lg overflow-hidden">
         <!--<a href="#" class="group w-full md:w-32 lg:w-48 h-48 md:h-full block self-start shrink-0 bg-gray-100 overflow-hidden relative">-->
         <!--  <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />-->
@@ -67,13 +57,13 @@
         <div class="flex flex-col gap-2 p-4 lg:p-6">
 
           <h2 class="text-gray-800 text-xl font-bold">
-            <a href="#" class="hover:text-indigo-500 active:text-indigo-600 transition duration-100">{{$place->name}}</a>
+            <a href="#" class="hover:text-indigo-500 active:text-indigo-600 transition duration-100">{{$favorite->place->name}}</a>
           </h2>
 
-          <p class="text-gray-500">{{$place->address}}</p>
+          <p class="text-gray-500">{{$favorite->place->address}}</p>
 
           <div>
-            <a href="/places/{{$place->id}}" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100">お店の詳細を見る</a>
+            <a href="/places/{{$favorite->place->id}}" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100">お店の詳細を見る</a>
           </div>
         </div>
       </div>
